3cae0a9e8badd6263c2a07108c83a7d1658fc830
angelomarcio40 <101677257+angelomarcio40@users.noreply.github.com>
2022-06-21 22:28:39 -0300

    Adiciionado JS

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Criar uma função em JavaScript
+// Formato ES6 baseado em Arrow Function
+// Função que realiza o cadastro do Livro
+const cadastrar = () =>{
+
+    // Captura o valor digitado no campo
+    let titulo = document.getElementById('titulo').value
+
+    alert(titulo)
+}
+
+// Forma de função padrão
+// function Cadastrar(){}
